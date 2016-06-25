cc8cf27b3f91e851a9328c9e46694fa8a27d59e8
louie <louie@louies-Mac.local>
2016-06-25 14:54:07 -0700

    Add view controller and methods

--- Silly Song/ViewController.swift
+++ Silly Song/ViewController.swift
@@ -10,8 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var lyricsView: UITextView!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        nameField.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -21,5 +30,44 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func reset(sender: AnyObject) {
+        nameField.text = ""
+        lyricsView.text = ""
+    }
+    
+    @IBAction func displayLyrics(sender: AnyObject) {
+        if nameField.text != "" {
+            lyricsView.text = lyricsForName(bananaFanaTemplate, fullName: nameField.text!)
+        }
+    }
+    
 }
 
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
+}
+
+func shortNameFromName(fullName: String) -> String {
+    let vowelSet = NSCharacterSet(charactersInString: "aeiouö")
+    let lowerCaseName = fullName.lowercaseString
+    
+    if let vowelRange = lowerCaseName.rangeOfCharacterFromSet(vowelSet) {
+        return lowerCaseName.substringFromIndex(vowelRange.startIndex)
+    }
+    
+    return lowerCaseName
+    
+}
+
+func lyricsForName(lyricsTemplate: String, fullName: String) -> String {
+    return lyricsTemplate.stringByReplacingOccurrencesOfString("<FULL_NAME>", withString: fullName).stringByReplacingOccurrencesOfString("<SHORT_NAME>", withString: shortNameFromName(fullName))
+}
+
+let bananaFanaTemplate = [
+    "<FULL_NAME>, <FULL_NAME>, Bo B<SHORT_NAME>",
+    "Banana Fana Fo F<SHORT_NAME>",
+    "Me My Mo M<SHORT_NAME>",
+    "<FULL_NAME>"].joinWithSeparator("\n")
