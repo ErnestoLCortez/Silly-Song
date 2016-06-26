0fae70db9386d3ea4b8965e50ad62e8405c583ef
louie <louie@louies-Mac.local>
2016-06-25 18:34:33 -0700

    Fix automatic layout

--- Silly Song/ViewController.swift
+++ Silly Song/ViewController.swift
@@ -24,11 +24,6 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
     @IBAction func reset(sender: AnyObject) {
         nameField.text = ""
